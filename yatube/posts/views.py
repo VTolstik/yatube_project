0dfa4f8c08198ebcfad56e3d0805ac720c14099d
Vladimir Tolstik <vltolstik@yandex.ru>
2023-02-07 00:34:00 +0300

    Джанго

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
+
 from django.shortcuts import render
 
 # Create your views here.
+
+# Главная страница
 def index(request):
-    return HttpResponse('А вот и главная страница')
+    template = 'posts/index.html'
+    return render(request, template) 
+
+# def index(request):
+#     return HttpResponse('А вот и главная страница')
 
 
 def group_post(request, slug):
